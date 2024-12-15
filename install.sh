cbc55e10c3e2236f82e04f4d98b5c1b36de647ed
Dylan Rubini <dylan.rubini@hotmail.co.uk>
2024-12-15 13:21:57 +0000

    [Building]

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+micromamba deactivate
+micromamba activate tf-cpu-source2
+pip uninstall scienceplots
+pip install .
+
+micromamba deactivate
 micromamba activate pfr-env
 pip uninstall scienceplots
 pip install .
